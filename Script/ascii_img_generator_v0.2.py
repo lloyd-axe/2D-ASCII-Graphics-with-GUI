a2cace34d50c37e405d98e60b762e32c9849b578
Lloyd Acha <lloydacha@gmail.com>
2020-08-30 18:37:53 +0800

    v0.2

--- Script/ascii_img_generator_v0.2.py
+++ Script/ascii_img_generator_v0.2.py
@@ -2,20 +2,33 @@ import PIL.Image
 from PIL import ImageTk, Image
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import messagebox
 #acii chars - in decending order in intensity
 ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
     
 #TKINTER
 root = Tk()
 root.title("ASCII IMAGE GENERATOR")
+root.resizable(width=False, height=False)
 #imgc = ImageTk.PhotoImage(PIL.Image.open('pic1.jpg'))
 #panel = Label(image = img)
 #panel.grid(row=0, column=0, columnspan=5)
-tit = Label(root, text ='Created By: Lloyd Acha')
+tit = Label(root, text ='By: Lloyd Acha')
 tit.grid(row=0, column=2)
 panel = Label(root)
 panel.grid(row=1, column=0, columnspan=3)
 
+def open_conv():
+    conv = Toplevel()
+    conv.title('ASCII GRAPHICS')
+    ins = Label(conv, text="A text file for this image has been created\non the same folder as the application")
+    ins.pack()
+    txt_b = Text(conv, width=120, height=120)
+    txt_b.pack()
+    with open("ascii_img.txt", "r") as file:
+       data = file.read()
+    txt_b.insert(INSERT,data)
+    messagebox.showinfo("CHEERS!", "Your ASCII Graphics has been generated!")
 
 def fileOpen():
     path_img = fd.askopenfilename(filetypes=[("Image File", ".jpg")])
@@ -27,7 +40,7 @@ def fileOpen():
     entry_b.insert(0,str(path))
                  
 #resize image
-def resize_img(image, new_width=100):
+def resize_img(image, new_width=120):
     width, height = image.size
     img_ratio =  height/width
     new_height = int(new_width * img_ratio)
@@ -45,7 +58,7 @@ def p_to_a(image):
     chars = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
     return chars
 
-def main(new_width = 100):
+def main(new_width = 120):
     #attemp to open image
     path = fd.askopenfilename(filetypes=[("Image File", ".jpg")])
     print(path)
@@ -74,10 +87,11 @@ def main(new_width = 100):
     #print(ascii_img)
 
     #save
-    with open("../ASCII File/ascii_img.txt", "w") as f:
+    with open("ascii_img.txt", "w") as f:
         f.write(ascii_img)
+    open_conv()
         
-img_btn = Button(text="Choose an Image Dear", width=30, command= main)
+img_btn = Button(text="Choose an Image", width=30, command= main)
 img_btn.grid(row=0,column=0)
 #main()
 root.mainloop()
